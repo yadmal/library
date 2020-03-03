da842a9230f9be72c56151afe3abcb01c93b926d
YashkinDmitry <yashkin23@gmail.com>
2020-03-03 16:20:19 +0300

    Getting a list of authors

---
+++ src/beens/Author.java
@@ -0,0 +1,16 @@
+package beens;
+
+public class Author {
+    private String name;
+
+    public Author() {
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
---
+++ src/beens/AuthorList.java
@@ -0,0 +1,49 @@
+package beens;
+
+import dataBase.Database;
+
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+
+public class AuthorList {
+    private ArrayList<Author> authorList = new ArrayList<>();
+
+    private ArrayList<Author> getAuthors(){
+        Statement stm = null;
+        ResultSet rs = null;
+        Connection conn = null;
+        try {
+            conn = Database.getConnection();
+            stm = conn.createStatement();
+            rs = stm.executeQuery("select * from author;");
+            while (rs.next()){
+                Author author = new Author();
+                author.setName(rs.getString("fio"));
+                authorList.add(author);
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                if(stm!=null) stm.close();
+                if(rs!=null) rs.close();
+                if(conn!=null) conn.close();
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+
+        }
+        return authorList;
+    }
+
+    public ArrayList<Author> getAuthorList(){
+        if(!authorList.isEmpty()){
+            return authorList;
+        } else {
+            return getAuthors();
+        }
+    }
+}
