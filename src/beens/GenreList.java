a93322c8c24333798f3ecdd8e7a29350a93fd8bd
YashkinDmitry <yashkin23@gmail.com>
2020-03-05 18:35:13 +0300

    Getting a list of Genre from DB

---
+++ src/beens/GenreList.java
@@ -0,0 +1,60 @@
+package beens;
+
+import dataBase.Database;
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+public class GenreList {
+
+    private ArrayList<Genre> genreList = new ArrayList<Genre>();
+
+    private ArrayList<Genre> getGenres() {
+        Statement stmt = null;
+        ResultSet rs = null;
+        Connection conn = null;
+        try {
+            conn = Database.getConnection();
+
+            stmt = conn.createStatement();
+            rs = stmt.executeQuery("select * from library.genre order by name");
+            while (rs.next()) {
+                Genre genre = new Genre();
+                genre.setName(rs.getString("name"));
+                genre.setId(rs.getLong("id"));
+                genreList.add(genre);
+            }
+
+        } catch (SQLException ex) {
+            Logger.getLogger(GenreList.class.getName()).log(Level.SEVERE, null, ex);
+        } finally {
+            try {
+                if (stmt != null) {
+                    stmt.close();
+                }
+                if (rs != null) {
+                    rs.close();
+                }
+                if (conn != null) {
+                    conn.close();
+                }
+            } catch (SQLException ex) {
+                Logger.getLogger(GenreList.class.getName()).log(Level.SEVERE, null, ex);
+            }
+        }
+
+        return genreList;
+    }
+
+    public ArrayList<Genre> getGenreList() {
+        if (!genreList.isEmpty()) {
+            return genreList;
+        } else {
+            return getGenres();
+        }
+    }
+}
